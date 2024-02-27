9dba26f75d6e4295826024b345d5db4bc738f64a
ashtons event pack <159561871+ashtonsevents@users.noreply.github.com>
2024-02-27 09:09:47 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -373,8 +373,7 @@ function downloadCertificate(certificateId) {
     const formattedDate = formatDate(rawDate); // Format date using custom function
 
     // Construct filename based on selected options, input value, and formatted date
-    const filename = `${names[certificateId]} ${certificateTemplate} ${rawDate.toISOString().slice(0, 10)}.pdf`;
-
+    const filename = `${names[certificateId]} ${certificate}.pdf`;
 
     const pdfOptions = {
         filename: filename, // Use the constructed filename
